6823ebdea87c61d2e4b8e62a27c9074134ee17a9
PWereh <wereh.achieng@gmail.com>
2025-04-03 15:44:32 +0300

    Add Financial Advisor agent implementation

---
+++ elementum_dsa/examples/financial_advisor/__init__.py
@@ -0,0 +1,9 @@
+"""Financial Advisor agent example for Elementum DSA framework."""
+
+from elementum_dsa.examples.financial_advisor.financial_advisor_agent import FinancialAdvisorAgent
+from elementum_dsa.examples.financial_advisor.financial_advisor_knowledge import FinancialAdvisorKnowledgeBase
+
+__all__ = [
+    'FinancialAdvisorAgent',
+    'FinancialAdvisorKnowledgeBase',
+]
