827a621bf18db427ba457b577f7ab4ad5f6758a0
PWereh <wereh.achieng@gmail.com>
2025-04-03 15:53:25 +0300

    Update main __init__.py to expose new implementation

--- elementum_dsa/__init__.py
+++ elementum_dsa/__init__.py
@@ -1,4 +1,11 @@
 """Elementum Domain-Specific Agent (DSA) Governance Framework."""
 
 __version__ = '1.0.0'
-__all__ = []
+
+from elementum_dsa.elementum_dsa import ElementumDSA, create_custom_agent, run_example
+
+__all__ = [
+    'ElementumDSA',
+    'create_custom_agent',
+    'run_example',
+]
